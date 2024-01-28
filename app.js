04330cd3a6d91bef6a82085d97cc5d3f5cba362f
Tu pkrgt32 <gamey305@gmail.com>
2024-01-27 19:16:51 -0600

    app ya esta lista falta el estilos

--- app.js
+++ app.js
@@ -1,11 +1,54 @@
 
+const botonE = document.getElementById("butE");
+const botonD = document.getElementById("butD");
+const botonC = document.getElementById("butC");
+let texFin = document.getElementById("textofinal");
+let texFinH4 = document.getElementById("texFinH4");
+let texFinP = document.getElementById("texFinP")
+let imgTexFinal = document.getElementById("imgTexFinal");
+let texInicio = document.getElementById("texInicio")
 
+botonE.addEventListener("click", function() {
+
+  if (texInicio.value !== ""){
+    
+    imgTexFinal.style.display = "none"; //style.display es una propiedad de JavaScript
+    texFinH4.style.display = "none"; //que controla la visibilidad de un elemento HTML.
+    texFinP.style.display = "none"; // si el valor es none el elenento se elimina 
+    texFin.style.display = "block" // si el valor es block el elemento se muestra este valor es el balor predeterminado
+    
+    
+  
+  }
+});
+
+botonD.addEventListener("click", function() {
+
+  if (texInicio.value !== ""){
+  
+    imgTexFinal.style.display = "none";
+    texFinH4.style.display = "none";
+    texFinP.style.display = "none";
+    texFin.style.display = "block" ;
+  
+  }
+});
+//copia texto
+botonC.addEventListener("click", function() {
+  //navigator.clipboard.writeText() se utilisa para comopiar 
+  navigator.clipboard.writeText(texFin.textContent);//texFin es el ID del elemento HTML que se va a copiar.
+ //textContent se utiliza para obtener el contenido de texto del elemento HTML.
+  imgTexFinal.style.display = "block";
+  texFinH4.style.display = "block";
+  texFinP.style.display = "block";
+  texFin.style.display = "none" ;
+});
 
 
 //este codigo tomo el texto del usuaro y lo modifican
 function inpTexE() { // la funsion esta en el botos para cuando se presione se ejecute el codigo
     let textod = document.getElementById('texInicio').value;// qui tomo el texto del usuario
-    
+
     let e = textod.replace(/e/g, 'enter');//aqui el texto del usuario se modifica 
     let i = e.replace(/i/g, "imes");//La “g” es un modificador que se utiliza para hacer una búsqueda global en lugar de una búsqueda única
     let a = i.replace(/a/g, "ai");// en pocas palagas busca en la palaba los carateres iguales para modificarlas
@@ -13,15 +56,17 @@ function inpTexE() { // la funsion esta en el botos para cuando se presione se e
     let u = o.replace(/u/g, "ufat");
   
     let elemento = document.getElementById('textofinal'); //qui se escribe el resutado del texto modificado
-  elemento.textContent = u;
- 
+    elemento.textContent = u;
 }
-  
-inpTexE()
 
+
+inpTexE();
+
+    
  
 function inpTexD() {
     let textod = document.getElementById('texInicio').value;
+    document.querySelector('#texInicio').value = '';
     let enter = textod.replace(/enter/g, "e");
     let imes = enter.replace(/imes/g, "i");
     let ai = imes.replace(/ai/g, "a");
@@ -30,5 +75,10 @@ function inpTexD() {
 
     let hola = document.getElementById('textofinal');
     hola.textContent = ufat;
+
   }
   inpTexD()
+
+  
+
+
